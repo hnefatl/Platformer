393162683ef1ee56e328de52a5763193ac9fabad
hnefal <hnefatl@gmail.com>
2013-12-30 17:11:56 +0000

    Initial

---
+++ Platformer/Platformer/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platformer
+{
+    public abstract class Entity
+    {
+
+
+        public Entity()
+        {
+
+        }
+    }
+}
---
+++ Platformer/Platformer/Level.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Platformer
+{
+    public class Level
+    {
+
+
+        public Level()
+        {
+
+        }
+    }
+}
